df09b8de8a5384aa76ef0298c8ae256ab30d5089
mubeen095 <148046624+mubeen095@users.noreply.github.com>
2026-05-23 13:26:40 +0530

    y4s

---
+++ app/head.js
@@ -0,0 +1,18 @@
+// src/app/head.js
+
+export default function Head() {
+    return (
+      <>
+        <title>THE CODING COMPANY | EdTech Platform</title>
+        <meta
+          name="description"
+          content="THE CODING COMPANY is a leading EdTech Platform offering Online Learning Solutions, Learning Management System (LMS), Digital Education, EdTech Solutions, and Educational Technology tools for modern learners"
+        />
+        <meta
+          name="google-site-verification"
+          content="TA7gCZBtleJ47YBQUv9ugHHaILDSyk3PjsbifJeETh4"
+        />
+      </>
+    );
+  }
+  
